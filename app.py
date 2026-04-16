2a68da6deb4d69dc7f816e93dfd6a85ab82c8da0
ch7alternativo-source <ch7alternativo@gmail.com>
2026-04-16 16:42:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -536,7 +536,7 @@ try:
                     f"{metricas_finales['tarjetas_partido']:.2f}",
                 ]
             }
-try:
+            
     st.table(pd.DataFrame(tabla))
 except Exception as e:
     st.error(f"Error: {e}")
